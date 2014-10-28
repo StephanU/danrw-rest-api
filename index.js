4a6f5755ec3f3b5a582de00e114fbea353b1f2fa
Stephan Uhle <stephanu@gmx.net>
2014-10-28 12:27:09 +0100

    Added handler for fetching the ingest status.

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+// add api handler
 app.post('/api/v1/ingest', oaisHandler.handleIngest);
+app.get('/api/v1/ingest/status', oaisHandler.handleIngestStatus);
 
+// start the server
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
-
 });
