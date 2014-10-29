12695e6fa3720433188e3af7c3f80431705fd2cd
Stephan Uhle <stephanu@gmx.net>
2014-10-29 21:23:10 +0100

    Added implementation for api path '/disseminate'.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.post('/api/v1/ingest', oaisHandler.handleIngest);
 app.get('/api/v1/ingest/status', oaisHandler.handleIngestStatus);
 app.get('/api/v1/order', oaisHandler.handleOrder);
 app.get('/api/v1/order/status', oaisHandler.handleOrderStatus);
+app.get('/api/v1/disseminate', oaisHandler.handleDissemination);
 
 // start the server
 var server = app.listen(3000, function () {
