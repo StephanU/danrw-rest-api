1f8d00066c487059a58667f537690757d3351eb3
Stephan Uhle <stephanu@gmx.net>
2014-10-29 11:59:59 +0100

    Added api path '/order' to order the dissemination of information.
    The DAWeb application needs a mix of basic authentication and cookie authentication (JSESSIONID) to accept the ordering of information. I didn't get this working using plain node.js functionality (due to an unknown reason the cookie was not set correctly when POSTing to '/automatedRetrieval/queueForRetrievalJSON') which is why I added the 'request' library.

--- index.js
+++ index.js
@@ -2,18 +2,15 @@ var express = require('express'),
 	app = express(),
 	oaisHandler = require('./lib/oaisHandler');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
 // add api handler
 app.post('/api/v1/ingest', oaisHandler.handleIngest);
 app.get('/api/v1/ingest/status', oaisHandler.handleIngestStatus);
+app.get('/api/v1/order', oaisHandler.handleOrder);
 
 // start the server
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('DA-NRW API listening at http://%s:%s', host, port);
 });
